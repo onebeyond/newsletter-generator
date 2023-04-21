663a9e0797accb1236b7273a03084d21ee10c105
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2023-04-21 21:39:17 +0200

    fix: deleted console.log

--- src/index.js
+++ src/index.js
@@ -8,6 +8,5 @@ const {
   const data = {}
   data.currentMonth = getCurrentMonth()
   const newsletter = await renderNewsletter({ data })
-  console.log('Newsletter', newsletter)
   saveNewsletter(newsletter)
 })()
