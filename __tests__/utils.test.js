d142ad76febb992f8f84147902ff500747fa54db
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2023-04-25 09:03:19 +0200

    feat: added StepSecurity information to the newsletter generated
    - Updated Newsletter template to reflect the changes in the Security section and others
    - Added new workflow to the main script
    - Updated tests

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,6 +1,7 @@
 
 const { renderNewsletter, getCurrentMonth, getTimeStamp } = require('../src/utils')
-
+const fixtures = require('../__fixtures__')
+const config = require('../src/config')
 jest.useFakeTimers().setSystemTime(new Date('2020-01-01'))
 
 describe('Utils', () => {
@@ -18,8 +19,14 @@ describe('Utils', () => {
 
   describe('renderNewsletter', () => {
     it('should render a newsletter', async () => {
-      const data = { currentMonth: 'January' }
-      const newsletter = await renderNewsletter({ data })
+      const data = { 
+        currentMonth: 'January',  
+        stepSecurity: {
+          top10: fixtures.stepSecurity.repositories.output.top10,
+          average: fixtures.stepSecurity.repositories.output.average
+        }
+      }
+      const newsletter = await renderNewsletter({ data, config })
       expect(newsletter).toMatchSnapshot()
     })
   })
--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ const {
   saveNewsletter,
   getCurrentMonth
 } = require('./utils')
+const debug = require('debug')('newsletter:main')
+
+const config = require('./config')
+const { collectStepSecurityReports } = require('./workflows/download-stepsecurity-report')
 
 ;(async () => {
   const data = {}
   data.currentMonth = getCurrentMonth()
-  const newsletter = await renderNewsletter({ data })
+  debug('Starting the StepSecurity process...')
+  data.stepSecurity = await collectStepSecurityReports()
+  debug('Rendering the newsletter...')
+  const newsletter = await renderNewsletter({ data, config })
+  debug('Storing the newsletter...')
   saveNewsletter(newsletter)
 })()
